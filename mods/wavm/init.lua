fbe75c7840c71978a488e46e7b5a3b1c66911f07
Bryan Rooke <rook275@ufl.edu>
2019-01-29 01:34:04 -0500

    WAVM lua script updated to work with filename argument

--- mods/wavm/init.lua
+++ mods/wavm/init.lua
@@ -1,8 +1,26 @@
+require 'os';
+require 'io';
+
 minetest.register_chatcommand("wavm", {
-	params = "",
-	description = "Printing Hello World",
-	func = function( _ )
-		minetest.chat_send_all("Hello World")
-		return true, "Hello World was successfully printed"
+	params = "<filename>",
+	description = "Calling WAVM Executable to run specified .wast filename",
+	func = function( _ , filename)
+		-- Get the working directory
+		local path = io.popen("cd")
+		local current_path = path:read("*a")
+		current_path = current_path:gsub("bin\n", "")
+		path:close()
+
+		-- Paths to executable and specified file
+		local execute_path = current_path .. "wavm_bin\\wavm-run.exe"
+		local filename_path = current_path .. "wavm_bin\\WAST_files\\" .. filename
+
+		-- Run executable with filename argument and print output to Minetest Chat
+		local handle = io.popen(execute_path .. " " .. filename_path)
+		local wavm_output = handle:read("*a")
+		handle:close()
+		minetest.chat_send_all(wavm_output)
+
+		return true, "WAVM Executable was successfully run with specified filename"
 	end,
 })
